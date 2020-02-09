d9c0af1e5abe36de1466ef199836f147a4cb34b7
mholwill <m_holwill@hotmail.co.uk>
2020-02-09 12:01:01 +0000

    initialized member class

---
+++ models/member.rb
@@ -0,0 +1,16 @@
+require_relative('../db/sql_runner')
+
+class Member
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @age = options['age'].to_i
+    @goal = options['goal']
+  end
+
+
+
+
+end
