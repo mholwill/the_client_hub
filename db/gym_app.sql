dd083554b769b499ce70af3b72e0e276eb01f609
mholwill <m_holwill@hotmail.co.uk>
2020-02-09 11:49:03 +0000

    added the_client_hub database

---
+++ db/gym_app.sql
@@ -0,0 +1,9 @@
+DROP TABLE bookings;
+DROP TABLE workouts;
+DROP TABLE members;
+
+CREATE TABLE members (
+  name VARCHAR(255),
+  age INT,
+  goal
+)
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: '', host: 'localhost' })
+      db = PG.connect({ dbname: 'the_client_hub', host: 'localhost' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
